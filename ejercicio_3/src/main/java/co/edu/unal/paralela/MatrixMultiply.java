c94e4f4e5e7c98da160a214a8782555fbf306893
Juan Rueda <juruedas@unal.edu.co>
2026-05-19 16:26:41 -0500

    feat: upload exercises 3,4 passing all the tests

---
+++ ejercicio_3/src/main/java/co/edu/unal/paralela/MatrixMultiply.java
@@ -0,0 +1,55 @@
+package co.edu.unal.paralela;
+
+import static edu.rice.pcdp.PCDP.forseq2d;
+import static edu.rice.pcdp.PCDP.forallChunked;
+import static edu.rice.pcdp.PCDP.forall;
+
+/**
+ * Clase envolvente pata implementar de forma eficiente la multiplicación dde matrices en paralelo.
+ */
+public final class MatrixMultiply {
+    /**
+     * Constructor por omisión.
+     */
+    private MatrixMultiply() {
+    }
+
+    /**
+     * Realiza una multiplicación de matrices bidimensionales (A x B = C) de forma secuencial.
+     *
+     * @param A Una matriz de entrada con dimensiones NxN
+     * @param B Una matriz de entrada con dimensiones NxN
+     * @param C Matriz de salida
+     * @param N Tamaño de las matrices de entrada
+     */
+    public static void seqMatrixMultiply(final double[][] A, final double[][] B,
+            final double[][] C, final int N) {
+        forseq2d(0, N - 1, 0, N - 1, (i, j) -> {
+            C[i][j] = 0.0;
+            for (int k = 0; k < N; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        });
+    }
+
+    /**
+     * Realiza una multiplicación de matrices bidimensionales (A x B = C) de forma paralela.
+     *
+     * @param A Una matriz de entrada con dimensiones NxN
+     * @param B Una matriz de entrada con dimensiones NxN
+     * @param C Matriz de salida
+     * @param N amaño de las matrices de entrada
+     */
+    public static void parMatrixMultiply(final double[][] A, final double[][] B,
+        final double[][] C, final int N) {
+            
+        forall(0, N - 1, (i) -> {
+            forall(0, N - 1, (j) -> {
+                C[i][j] = 0.0;
+                for (int k = 0; k < N; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                };
+            });
+    });
+    }
+}
